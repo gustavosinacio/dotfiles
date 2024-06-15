808d3261d210b6104e44be727f6bd2cc2c454f7c
Gustavo Inácio <gsinacio94@gmail.com>
2024-06-15 13:33:25 -0300

    adds lsp and formatting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,14 +7,14 @@ return {
     ensure_installed = {"lua", "vim", "vimdoc"},
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+    sync_install = true,
 
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
 
     -- List of parsers to ignore installing (or "all")
-    ignore_install = { "javascript" },
+    -- ignore_install = { "javascript" },
 
     ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
     -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
@@ -41,6 +41,7 @@ return {
       -- Instead of true it can also be a list of languages
       additional_vim_regex_highlighting = false,
     },
+    indent = { enable = true }
   }
   end
 }
