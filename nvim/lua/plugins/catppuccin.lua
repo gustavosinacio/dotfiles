e6b7e00c004d5765fdaefbd855ecc12d13fde278
Gustavo Inácio <gsinacio94@gmail.com>
2024-06-22 12:55:49 -0300

    adds tokyodark theme and cokeline

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,50 +1,57 @@
 return {
-	"catppuccin/nvim",
-	lazy = false,
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			transparent_background = true,
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				treesitter = true,
-				harpoon = true,
-				telescope = true,
-				mason = true,
-				noice = true,
-				notify = true,
-				which_key = true,
-				fidget = true,
-				native_lsp = {
-					enabled = true,
-					virtual_text = {
-						errors = { "italic" },
-						hints = { "italic" },
-						warnings = { "italic" },
-						information = { "italic" },
-					},
-					underlines = {
-						errors = { "underline" },
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      transparent_background = false,
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        harpoon = true,
+        telescope = true,
+        mason = true,
+        noice = true,
+        notify = true,
+        which_key = true,
+        fidget = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
 
-						hints = { "underline" },
-						warnings = { "underline" },
-						information = { "underline" },
-					},
-					inlay_hints = {
-						background = true,
-					},
-				},
-				mini = {
-					enabled = true,
-					indentscope_color = "",
-				},
-			},
-		})
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
+    })
 
-		-- setup must be called before loading
-		-- vim.cmd.colorscheme("catppuccin-mocha")
-	end,
+    -- setup must be called before loading
+    -- vim.cmd.colorscheme("catppuccin-mocha")
+  end,
 }
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,15 +1,15 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "tokyonight",
-			},
-		})
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "tokyodark",
+      },
+    })
 
-		vim.keymap.set("n", "<leader>hl", function()
-			require("lualine").get_config()
-		end)
-	end,
+    vim.keymap.set("n", "<leader>hl", function()
+      require("lualine").get_config()
+    end)
+  end,
 }
