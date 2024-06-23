84a1f24efd35c1aa44d649434a35e0ced2bef47a
Gustavo Inácio <gsinacio94@gmail.com>
2024-06-23 20:02:30 -0300

    adds config for conditional detachment of nvim-dap-go

--- nvim/lua/plugins/DAP-go.lua
+++ nvim/lua/plugins/DAP-go.lua
@@ -1,10 +1,22 @@
 return {
 	"leoluz/nvim-dap-go",
+	ft = "go",
 	dependencies = {
 		"mfussenegger/nvim-dap",
 		"rcarriga/nvim-dap-ui",
 	},
-	config = function()
-		require("dap-go").setup()
+	opts = {
+		delve = {
+			detached = (function()
+				if vim.fn.has("win32") == 1 then
+					return false
+				end
+				return true
+			end)(),
+		},
+	},
+	config = function(_, opts)
+		require("dap-go").setup(opts)
+		require("dap").set_log_level("TRACE")
 	end,
 }
