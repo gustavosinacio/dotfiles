3960d74f82f0a3fc1cbd39c268520f00e8cdecba
Gustavo Inácio <gsinacio94@gmail.com>
2024-07-01 22:03:47 -0300

    changes terminal open shortcut to cmd ctrl return

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -14,4 +14,26 @@ return {
       },
     },
   },
+  config = function()
+    require("obsidian").setup({
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents",
+        },
+      },
+    })
+
+    -- Create an augroup to manage the autocmds
+    vim.api.nvim_create_augroup("FiletypeSettings", { clear = true })
+
+    -- Create an autocmd for Python files
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "markdown",
+      callback = function()
+        vim.opt_local.conceallevel = 1
+      end,
+      group = "FiletypeSettings",
+    })
+  end,
 }
