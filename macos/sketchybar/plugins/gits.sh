b01fe8430396134705daf18acf4df1b0f5be88e1
Gustavo Inácio <gsinacio94@gmail.com>
2024-07-06 13:08:18 -0300

    adds obsidian folder to sketchybar

--- macos/sketchybar/plugins/gits.sh
+++ macos/sketchybar/plugins/gits.sh
@@ -4,4 +4,5 @@ sketchybar --set "$NAME" \
   label="\
 $($HOME/i3life/scripts/git-file-report.sh $HOME/dotfiles)\
 $($HOME/i3life/scripts/git-file-report.sh $HOME/i3life)\
+$($HOME/i3life/scripts/git-file-report.sh $HOME/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents)\
 ."
