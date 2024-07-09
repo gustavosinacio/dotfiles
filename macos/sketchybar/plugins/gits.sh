0aef5adf9b8ae1a301aaac19710406d5e85c8b34
Gustavo Inácio <gsinacio94@gmail.com>
2024-07-09 01:11:47 -0300

    adjusts sketchybar configs

--- macos/sketchybar/plugins/gits.sh
+++ macos/sketchybar/plugins/gits.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 sketchybar --set "$NAME" \
+  icon=" " \
+  icon.padding_left=10 \
+  background.padding_left=8 \
+  background.padding_right=8 \
+  background.corner_radius=4 \
   label="\
-$($HOME/i3life/scripts/git-file-report.sh $HOME/dotfiles)\
-$($HOME/i3life/scripts/git-file-report.sh $HOME/i3life)\
-$($HOME/i3life/scripts/git-file-report.sh $HOME/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents)\
-."
+$(
+    $HOME/i3life/scripts/git-file-report.sh \
+      $HOME/dotfiles \
+      $HOME/i3life \
+      $HOME/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents
+  ) \
+"
