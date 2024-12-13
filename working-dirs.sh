5894bb60f7a24f63091b287d726d76d006f766dc
Gustavo Inácio <gsinacio94@gmail.com>
2024-12-12 23:45:12 -0300

    adds option to use local optional dirs

--- working-dirs.sh
+++ working-dirs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 export WORKING_DIRS=(
   "$HOME/github"
   "$HOME/workhub"
@@ -8,3 +9,10 @@ export WORKING_DIRS=(
   "$HOME/obsidian"
   "$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents"
 )
+
+if [ -f ~/local_dirs.sh ]; then
+  source ~/local_dirs.sh
+  WORKING_DIRS+=(${LOCAL_DIRS[@]})
+fi
+
+WORKING_DIRS=($(printf "%s\n" "${WORKING_DIRS[@]}" | sort -u))
