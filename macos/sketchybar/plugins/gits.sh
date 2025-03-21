339360286caf1be25ff1102761669855cc770040
Gustavo Inácio <gsinacio94@gmail.com>
2025-03-20 23:58:48 -0300

    adds sketchybar configs

--- macos/sketchybar/plugins/gits.sh
+++ macos/sketchybar/plugins/gits.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
 sketchybar --set "$NAME" \
-	icon=" " \
-	icon.padding_left=10 \
-	icon.color=0xffF17013 \
-	background.padding_left=8 \
-	background.padding_right=8 \
-	background.corner_radius=4 \
    label="$($HOME/unixverse/scripts/git-file-report.sh)"
