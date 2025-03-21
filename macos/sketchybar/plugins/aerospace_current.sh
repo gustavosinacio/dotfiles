2ba6a60f6f22a52a905aa8dc598e2057d2c40e2b
Gustavo Inácio <gsinacio94@gmail.com>
2025-03-21 17:06:34 -0300

    adds aerospace, karabiner, yazi configs

--- macos/sketchybar/plugins/aerospace_current.sh
+++ macos/sketchybar/plugins/aerospace_current.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-# Get the focused workspace
 FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
 
+if [[ $FOCUSED_WORKSPACE =~ ^[1-9]$ ]]; then
+  case $FOCUSED_WORKSPACE in
+    1) ICON="" ;;
+    2) ICON="" ;;
+    3) ICON="" ;;
+    4) ICON="" ;;
+    5) ICON="󰎱" ;;
+    6) ICON="󰎳" ;;
+    7) ICON="󰎶" ;;
+    8) ICON="󰎹" ;;
+    9) ICON="" ;;
+    "10") ICON="" ;;
+    E) ICON="" ;;
+  esac
+else
+  ICON="" 
+fi
+
 # Update the sketchybar item
-sketchybar --set current label="$FOCUSED_WORKSPACE"
+sketchybar --set current label="$FOCUSED_WORKSPACE" icon="$ICON"
